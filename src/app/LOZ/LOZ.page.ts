ba768debc007842a39035eaf29fbf3475519e594
erik <erik.hauer@outlook.de>
2025-03-30 17:30:12 +0200

    8- Hauer (hab den Fehler gefunden)

--- src/app/LOZ/LOZ.page.ts
+++ src/app/LOZ/LOZ.page.ts
@@ -1,23 +1,43 @@
-import {Component, OnInit} from '@angular/core';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonItem} from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // <-- Hier importieren
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonThumbnail
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-import {LOZApiService} from "../LOZ.api/LOZ.api.service";
+import { HttpClientModule } from '@angular/common/http';
+import { LOZApiService } from '../LOZ.api/LOZ.api.service';
 
 @Component({
   selector: 'app-LOZ',
   templateUrl: 'LOZ.page.html',
   styleUrls: ['LOZ.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, HttpClientModule, IonItem]
+  standalone: true,
+  imports: [
+    CommonModule, // <-- Füge das hier hinzu
+    IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, HttpClientModule, IonItem, IonLabel, IonList, IonThumbnail
+  ],
+  providers: [LOZApiService]
 })
 export class LOZPage implements OnInit {
+  monsters: any[] = [];
 
-  constructor(private lOZApiService:LOZApiService) {}
-   ngOnInit() {
+  constructor(private lOZApiService: LOZApiService) {}
+
+  ngOnInit() {
     this.loadMonsters();
   }
-  loadMonsters() {
-    this.lOZApiService.getAllMonsters().subscribe()
-  }
 
+  loadMonsters() {
+    this.lOZApiService.getAllMonsters().subscribe((data: any) => {
+      console.log('API Response:', data); // API-Daten prüfen
+      this.monsters = data.data;
+    });
+  }
 }
