dcc5d791c6e33a3a37c3f5dd5fe74eaaf5abd2bd
vicik <210042@studierende.htl-donaustadt.at>
2025-04-07 12:01:33 +0200

    23- Kovacic

---
+++ src/app/monster-page-loz/monster-page-loz.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-monster-page-loz',
+  templateUrl: './monster-page-loz.component.html',
+  styleUrls: ['./monster-page-loz.component.scss'],
+})
+export class MonsterPageLOZComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/weapon-page-loz/weapon-page-loz.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weapon-page-loz',
+  templateUrl: './weapon-page-loz.component.html',
+  styleUrls: ['./weapon-page-loz.component.scss'],
+})
+export class WeaponPageLOZComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
