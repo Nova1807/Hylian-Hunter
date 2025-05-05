8cd47c745413aa3a9546f92229d82cf50a099a9f
erik <erik.hauer@outlook.de>
2025-05-05 09:11:22 +0200

    26 -v3 28 -v3 -Hauer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import {IonButtons, IonHeader, IonTitle, IonToolbar} from "@ionic/angular/standalone";
+import { IonButtons, IonHeader, IonTitle, IonToolbar } from "@ionic/angular/standalone";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   imports: [
+    CommonModule,
     IonHeader,
     IonToolbar,
     IonButtons,
     IonTitle
   ]
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
   title = 'HylianHunter';
   activePage: string = 'default';
 
@@ -25,16 +27,22 @@ export class HeaderComponent {
     });
   }
 
+  ngAfterViewInit() {
+    // Erzwingt eine Neuberechnung der Styles nach dem Laden
+    setTimeout(() => {
+      window.dispatchEvent(new Event('resize'));
+    }, 100);
+  }
+
   updateHeader(url: string) {
     if (url.includes('tab3')) {
-      this.activePage = 'home';
+      this.activePage = 'tab3';
     } else if (url.includes('LOZ')) {
       this.activePage = 'LOZ';
     } else if (url.includes('Monster-Hunter')) {
-      this.activePage = 'monsters';
+      this.activePage = 'Monster-Hunter';
     } else {
       this.activePage = 'default';
     }
-    console.log('Active Page:', this.activePage); // Debugging
   }
 }
