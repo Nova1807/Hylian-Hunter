7c02b5c8096111cbc8495f99fb3266ad22c686a9
vicikoc <2100042@studierende.htl-donaustadt.at>
2025-06-05 23:33:24 +0200

    53 -Kovacic

--- src/app/LOZ/LOZ.page.ts
+++ src/app/LOZ/LOZ.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'; // <-- Hier importieren
+import { CommonModule } from '@angular/common';
 import {
   IonHeader,
   IonToolbar,
@@ -8,13 +8,23 @@ import {
   IonItem,
   IonLabel,
   IonList,
-  IonThumbnail, IonCardContent, IonCardTitle, IonCard, IonCardHeader, IonButton, IonSearchbar
+  IonThumbnail,
+  IonCardContent,
+  IonCardTitle,
+  IonCard,
+  IonCardHeader,
+  IonButton,
+  IonSearchbar,
+  IonRange,
+  IonSelect,
+  IonSelectOption,
+  IonInput
 } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { LOZApiService } from '../LOZ.api/LOZ.api.service';
-import {RouterLink} from "@angular/router";
-import {HeaderComponent} from "../header/header.component";
+import { Router, RouterLink } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-LOZ',
@@ -22,25 +32,83 @@ import {HeaderComponent} from "../header/header.component";
   styleUrls: ['LOZ.page.scss'],
   standalone: true,
   imports: [
-    CommonModule, // <-- Füge das hier hinzu
-    IonHeader, IonToolbar, IonTitle, IonContent, HttpClientModule, IonList, IonCardContent, IonCardTitle, IonCard, IonCardHeader, IonButton, RouterLink, IonSearchbar, HeaderComponent
+    CommonModule,
+    FormsModule,
+    IonHeader, IonToolbar, IonTitle, IonContent,
+    HttpClientModule, IonList, IonCardContent, IonCardTitle,
+    IonCard, IonCardHeader, IonButton, RouterLink,
+    IonSearchbar, IonRange, IonSelect, IonSelectOption, IonInput,
+    HeaderComponent
   ],
   providers: [LOZApiService]
 })
 export class LOZPage implements OnInit {
-  monsters: any[] = [];
+  weapons: any[] = [];
+  allWeapons: any[] = [];
 
-  constructor(private lOZApiService: LOZApiService) {}
+  searchTerm: string = '';
+  selectedPlace: string = '';
+  minAttack: number | null = null;
+  minDefense: number | null = null;
+  availablePlaces: string[] = [];
+  showFilters: boolean = false;
+
+  constructor(
+    private lOZApiService: LOZApiService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.loadMonsters();
+    this.loadWeapons();
   }
 
-  loadMonsters() {
-    this.lOZApiService.getAllMonsters().subscribe((data: any) => {
-      console.log('API Response:', data); // API-Daten prüfen
-      this.monsters = data.data;
+  loadWeapons() {
+    this.lOZApiService.getAllWeapons().subscribe((data: any) => {
+      console.log('API Response:', data);
+
+      this.allWeapons = data.data.map((w: any) => ({
+        id: w.id,
+        name: w.name,
+        image: w.image,
+        place: w.common_locations?.[0] || 'Unknown',
+        attack: w.properties?.attack ?? 0,
+        defense: w.properties?.defense ?? 0
+      }));
+
+      this.availablePlaces = [...new Set(this.allWeapons.map(w => w.place))];
+      this.applyFilters();
     });
   }
 
+  applyFilters() {
+    const term = this.searchTerm.toLowerCase().trim();
+
+    this.weapons = this.allWeapons.filter(weapon => {
+      const matchesSearch = weapon.name.toLowerCase().includes(term);
+      const matchesPlace = this.selectedPlace ? weapon.place === this.selectedPlace : true;
+      const matchesAttack = this.minAttack != null ? weapon.attack >= this.minAttack : true;
+      const matchesDefense = this.minDefense != null ? weapon.defense >= this.minDefense : true;
+
+      return matchesSearch && matchesPlace && matchesAttack && matchesDefense;
+    });
+  }
+
+  toggleFilter() {
+    this.showFilters = !this.showFilters;
+  }
+
+  resetFilters() {
+    this.searchTerm = '';
+    this.selectedPlace = '';
+    this.minAttack = null;
+    this.minDefense = null;
+    this.applyFilters();
+  }
+
+  navigateToDetail(weaponId: number) {
+    this.router.navigate(['/weapon-detail'], {
+      queryParams: { id: weaponId },
+      queryParamsHandling: 'merge'
+    });
+  }
 }
